b1f677af3bc3ba936e18a640a29d2584a07d1c2d
Jashandeep Singh <jashandeep1659@gmail.com>
2026-01-04 16:50:24 +0530

    feat: colors extraction

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import { color } from "bun";
 import { chromium } from "playwright";
 
 console.log(`Firing up the brandkit scrapper :)`);
@@ -17,11 +18,33 @@ const scrapBrandKit = async () => {
       "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 Chrome/120 Safari/537.36",
   });
   const page = await context.newPage();
-  await page.goto("https://jashan.dev", {
-    waitUntil: "networkidle",
+  await page.goto("https://stripe.com/in", {
+    // waitUntil: "networkidle",
   });
-  const title = await page.title();
-  console.log(title);
+  await new Promise<void>((res) => {
+    setTimeout(res, 5000);
+  });
+  console.log("Main work started");
+  const colors = await page.evaluate(() => {
+    const map = new Map<string, number>();
+
+    const add = (color: string) => {
+      if (!color || color === "transparent" || color === "rgba(0, 0, 0, 0)")
+        return;
+      map.set(color, (map.get(color) ?? 0) + 1);
+    };
+    document.querySelectorAll("*").forEach((el) => {
+      const cs = getComputedStyle(el);
+      add(cs.color);
+      add(cs.backgroundColor);
+    });
+
+    return Array.from(map.entries())
+      .map(([color, count]) => ({ color, count }))
+      .sort((a, b) => b.count - a.count);
+  });
+
+  console.log(colors);
   await page.screenshot({
     path: "secondary.png",
   });
